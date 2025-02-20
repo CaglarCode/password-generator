b62cfb907cf07106ef097b8f9e69113af2063264
caglar togan <ctogan@gmail.com>
2025-02-20 11:44:00 -0500

    Delete passwordgenerator.py

--- passwordgenerator.py
+++
@@ -1,8 +0,0 @@
-import secrets
-import string
-
-def generate_secure_key(length=43):
-    characters = string.ascii_letters + string.digits + "+/="
-    return ''.join(secrets.choice(characters) for _ in range(length))
-
-print("Generated Key:", generate_secure_key())
